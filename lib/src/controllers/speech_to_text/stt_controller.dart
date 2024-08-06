2e26b80724001e73ca2eb4fc7b364a119bb53849
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-08-06 23:02:35 +0700

    Implemented STT controller

--- lib/src/controllers/speech_to_text/stt_controller.dart
+++ lib/src/controllers/speech_to_text/stt_controller.dart
@@ -1,3 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
-class SttController extends ChangeNotifier {}
+class SttController extends ChangeNotifier {
+  final SpeechToText _speechToText = SpeechToText();
+
+  String _text = '';
+  String get text => _text;
+
+  bool _isListening = false;
+  bool get isListening => _isListening;
+
+  Future<bool> listen() async {
+    if (isListening) {
+      _isListening = false;
+      _speechToText.stop();
+      return true;
+    }
+
+    if (!await _speechToText.initialize()) {
+      return false;
+    }
+
+    _text = '';
+    _isListening = true;
+    _speechToText.listen(
+      onResult: (SpeechRecognitionResult result) {
+        _text = result.recognizedWords;
+      },
+    );
+
+    return true;
+  }
+}
