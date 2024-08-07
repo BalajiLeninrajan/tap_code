252f0fd331a80f80f1cf6ead9ebd67fa1dd93492
Balaji Leninrajan <bleninrajan@uwaterloo.ca>
2024-08-07 20:27:33 +0700

    Created basic UI for translation cards

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:tap_code/src/controllers/speech_to_text/stt_controller.dart';
-import 'package:tap_code/src/widgets/bottom_controls.dart';
 
+import 'package:tap_code/src/widgets/bottom_controls.dart';
 import 'package:tap_code/src/widgets/home_app_bar.dart';
 import 'package:tap_code/src/widgets/recording_button.dart';
+import 'package:tap_code/src/widgets/translation_cards.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key, required this.sttController});
@@ -16,7 +17,7 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const HomeAppBar(),
-      body: const Placeholder(),
+      body: const TranslationCards(),
       bottomNavigationBar: const BottomControls(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: RecordingButton(sttController: sttController),
---
+++ lib/src/widgets/translation_cards.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class TranslationCards extends StatefulWidget {
+  const TranslationCards({super.key});
+
+  @override
+  State<TranslationCards> createState() => _TranslationCardsState();
+}
+
+class _TranslationCardsState extends State<TranslationCards> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Card(
+              color: Theme.of(context).colorScheme.primary,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Text(
+                    'Card 1',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onPrimary,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Expanded(
+            child: Card(
+              color: Theme.of(context).colorScheme.secondary,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Text(
+                    'Card 2',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.onSecondary,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
